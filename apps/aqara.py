de13a96b204172fe9125aa3b878f2530fcfc826d
Werner Pieterson <werner@werner.co.za>
2020-08-10 22:18:05 +0200

    Fix issue where attributes lost

--- apps/aqara.py
+++ apps/aqara.py
@@ -16,17 +16,16 @@ class Aqara(hass.Hass):
     """Initialize the Aqara app."""
     motion_sensors = self.args.get(CONF_MOTION_SENSORS, [])
 
-    handles = []
     for entity_id in motion_sensors:
-      handle = self.listen_event(self.motion_sensor_state_on, "xiaomi_aqara.motion", entity_id=entity_id)
-      handles.append(handle)
+      self.listen_event(self.motion_sensor_state_on, "xiaomi_aqara.motion", entity_id=entity_id)
 
   def motion_sensor_state_on(self, event, data, kwargs):
     """Set motion sensor state to on"""
     entity_id = kwargs.get("entity_id")
     if not entity_id:
       return
-    self.set_state(entity_id, state="on")
+    state = self.get_state(entity_id, attribute="all")
+    self.set_state(entity_id, state="on", attributes=state.get("attributes", {}))
 
     timeout = self.args.get(CONF_TIMEOUT, DEFAULT_TIMEOUT)
     self.run_in(self.motion_sensor_state_off, timeout, entity_id=entity_id)
@@ -36,4 +35,5 @@ class Aqara(hass.Hass):
     entity_id = kwargs.get("entity_id")
     if not entity_id:
       return
-    self.set_state(entity_id, state="off")
+    state = self.get_state(entity_id, attribute="all")
+    self.set_state(entity_id, state="off", attributes=state.get("attributes", {}))
